8743547b66aa80911ce66f0e5191b09a945cd06e
Hamza Keurti <hamza.keurti@gmail.com>
2019-05-07 13:59:13 +0800

    write tree parser

--- src/load.py
+++ src/load.py
@@ -1,15 +1,38 @@
 import numpy as np
+import re
+
+GLOVE = "../data/glove.6B.300d.txt"
+TREES_DIR = "../data/trainDevTestTrees_PTB/"
+STANFORD_DIR = "../data/stanfordSentimentTreebank/"
+TREES_T = TREES_DIR + "train.txt"
+TREES_D = TREES_DIR + "dev.txt"
+TREES_S = TREES_DIR + "test.txt"
+
+RE_WORD = re.compile(' (\w+)\)')
 
 
 # Taken from answer https://stackoverflow.com/questions/37793118/load-pretrained-glove-vectors-in-python
-def loadGloveEmbedding(gloveFile):
+def load_glove_embedding(glove_file):
     print("Loading Glove Model")
-    f = open(gloveFile,'r')
-    model = {}
-    for line in f:
-        splitLine = line.split()
-        word = splitLine[0]
-        embedding = np.array([float(val) for val in splitLine[1:]])
-        model[word] = embedding
-    print("Done.",len(model)," words loaded!")
+    with open(glove_file, 'r', encoding="utf8") as f:
+        model = {}
+        for line in f:
+            split_line = line.split()
+            word = split_line[0]
+            embedding = np.array([float(val) for val in split_line[1:]])
+            model[word] = embedding
+    print("Done.", len(model), " words loaded!")
     return model
+
+
+def parse_tree(tree_file):
+    sentences = []
+    labels = []
+    with open(tree_file,'r', encoding="utf8") as f:
+        for line in f:
+            found = re.findall(RE_WORD, line)
+            if (not found):
+                continue
+            sentences.append(np.char.lower(found))
+            labels.append(line[1])
+    return sentences, labels
