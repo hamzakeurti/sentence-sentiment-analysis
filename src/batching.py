ff14e01aa2515332f7096dd742fcb03093eada80
Hamza Keurti <hamza.keurti@gmail.com>
2019-05-08 21:23:50 +0800

    A batcher without padding

--- src/batching.py
+++ src/batching.py
@@ -21,6 +21,27 @@ def batch_generator(sentences, labels, embeddings, batch_size, shuffle=True):
         yield tensor_sentences, tensor_labels
 
 
+def iterate_data(sentences, labels, embeddings, batch_size, shuffle= True):
+    indx = list(range(len(labels)))
+    if shuffle:
+        np.random.shuffle(indx)
+
+    for start_idx in range(0, len(labels), batch_size):
+        end_idx = min(start_idx + batch_size, len(labels))
+        batch_sentences, batch_labels = sentences[indx[start_idx: end_idx]], labels[indx[start_idx: end_idx]]
+
+        batch_sentences, batch_labels = sort_batch(batch_sentences, batch_labels)
+
+        tensor_labels = torch.tensor(batch_labels)
+        tensor_sentences = embed(batch_sentences, embeddings)
+        yield tensor_sentences, tensor_labels # Ordered sequences of tensorized embedded sentences and labels.
+
+
+def sort_batch(batch_sentences, batch_labels):
+    lengths = np.array([len(sentence) for sentence in batch_sentences])
+    idx = np.argsort(-lengths)
+    return batch_sentences[idx], batch_labels[idx]
+
 def pad_batch(batch_sentences):
     lengths = [len(sentence) for sentence in batch_sentences]
     padded = np.chararray((len(lengths), max(lengths)))
@@ -31,9 +52,9 @@ def pad_batch(batch_sentences):
     return padded
 
 
-def embed(padded_sentences, embeddings):
+def embed(batch_sentences, embeddings):
     sentence_embeddings = []
-    for sentence in padded_sentences:
+    for sentence in batch_sentences:
         sentence_embedding = [embeddings.get(word, embeddings[UNK]) for word in sentence]
         sentence_embeddings.append(torch.tensor(sentence_embedding))
     return torch.stack(sentence_embedding)
